d21a4309ec811fd4a6bc8c3203abb3ac3a5970e7
DragonStuff <NICHOAL015@ignatius.sa.edu.au>
2014-08-01 13:58:54 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 echo "Grabbing Python 3 and installing it into py3env...."
 virtualenv -p /usr/bin/python3.3 ~/py3env
 echo "Setting default Python version to latest...."
-echo 'source py3env/bin/activate' >> ~/.bash_profile
+echo 'source ~/py3env/bin/activate' >> ~/.bash_profile
 echo "Downloading the examples..."
 wget --content-disposition "http://sporty-pup-52-131689.apse2.nitrousbox.com/examples.zip"
 echo "Done, extracting..."
@@ -12,6 +12,6 @@ echo "Removing the downloaded zip file..."
 rm examples.zip
 echo "Examples ready, done..."
 echo "Dropping you into a shell with Python 3 set to default..."
-source ~/py3env/bin/activate
+source . ~/py3env/bin/activate
 echo "Done. Please close this console and open a new one!"
 exit
